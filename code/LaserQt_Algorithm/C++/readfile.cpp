fc0e00068e5ba75eb05adb1b39681833b7d8518a
Iamluffy <IamLJT@users.noreply.github.com>
2016-11-13 12:45:55 +0800

    LJT\修正

--- code/LaserQt_Algorithm/C++/readfile.cpp
+++ code/LaserQt_Algorithm/C++/readfile.cpp
@@ -1,32 +1,46 @@
 #include "readfile.h"
 
-/*
- * 函数名：ReadFile
- * 功能简介： 读取点云数据
- * 输入参数： path - ; numOfPoints - 
- * 输出参数： M - 
-*/
-double* ReadFile(const char* path, int32_t& numOfPoints) {
-	FILE* fp;
-	fp = fopen(path, "r");
-	std::vector<double> dataVector;
+extern "C" __declspec(dllexport) double* ReadFile(char* strPath, int32_t& num, int32_t& dim)	
+	// Read file data one by one
+{
+	/*
+	std::ifstream fin(strPath);
+	std::vector<double> vec;
+	while(fin)
+	{
+		double idata;
+		fin >> idata;
+		vec.push_back(idata);	//	can't read?
+	}	*/
+	FILE *fp;
+	fp = fopen(strPath, "r");
+	std::vector<double> vec;
+	num = 0;
+	while(!feof(fp))
+	{
+		double idata1, idata2, idata3;
+		fscanf(fp, "%lf,%lf,%lf", &idata1,&idata2,&idata3);
+		dim=3;
+		vec.push_back(idata1);
+		vec.push_back(idata2);
+		vec.push_back(idata3);
+		num++;
+	}
+	double *M = new double[(int)vec.size()];
+	for(int i=0; i<(int)vec.size(); i++)
+		M[i]=vec[i];
+	//num=(int)vec.size()/3;		//	icp need it!
+	fclose(fp);
+	return M;
+}
 
-	while (!feof(fp)) {
-		double input_data01, input_data02, input_data03;
-		fscanf(fp, "%lf, %lf, %lf", &input_data01, &input_data02, &input_data03);
-		dataVector.push_back(input_data01);
-		dataVector.push_back(input_data02);
-		dataVector.push_back(input_data03);
+extern "C" __declspec(dllexport) void WriteFile(char* strPath, double* M, int32_t num, int32_t dim)
+{
+	FILE *fp;
+	fp = fopen(strPath, "w");
+	for(int i=0; i<num*dim; i+=dim)
+	{
+		fprintf(fp, "%lf,%lf,%lf\n", M[i], M[i+1], M[i+2]);
 	}
 	fclose(fp);
-
-	int size = (int)dataVector.size();
-	double* dataMatrix = new double[size];
-	for (int i = 0; i < size; i++) {
-		dataMatrix[i] = dataVector[i];
-	}
-		
-	numOfPoints = size / 3;
-	
-	return dataMatrix;
 }
--- code/LaserQt_Algorithm/C++/readfile.h
+++ code/LaserQt_Algorithm/C++/readfile.h
@@ -1,10 +1,13 @@
-#ifndef __READFILE_H__
-#define __READFILE_H__
+#ifndef READFILE_H
+#define READFILE_H
+
+#pragma warning(disable:4996)
 
 #include <fstream>
 #include <vector>
 #include "matrix.h"
 
-double* ReadFile(const char* strPath, int32_t& num);
+extern "C" __declspec(dllexport) double* ReadFile(char* strPath, int32_t& num, int32_t& dim);
+extern "C" __declspec(dllexport) void WriteFile(char* Path, double* M, int32_t num, int32_t dim);
 
-#endif  // __READFILE_H__
+#endif
